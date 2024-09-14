d6f9cf5deff6570270e726e1a4c168bc63a56524
Luis Vilar <luis_velazquez@etudante.sesisenai.org.br>
2024-09-14 01:05:49 -0300

    Refactor userPublicRouter to userRouter

--- src/routes/v1/users.routes.ts
+++ src/routes/v1/users.routes.ts
@@ -4,19 +4,10 @@ import { getAllUsers, createNewUser } from "../../controllers/user.controllers";
 const userPublicRouter = Router();
 const userPrivateRouter = Router();
 
-
 //PRIVATE ROUTES
-userPrivateRouter.use((req, res, next) => {
-  console.log("Private route accessed by" + req.ip);
-  //todo: implement jwt middleware and RBAC
-  next();
-});
 userPrivateRouter.get("/users", getAllUsers);
 
-
 //PUBLICS ROUTES
-userPublicRouter.post("/users", (req, res) => {
-  createNewUser(req, res);
-});
+userPublicRouter.post("/users", createNewUser);
 
-export default [userPublicRouter, userPrivateRouter];
+export { userPublicRouter, userPrivateRouter };
