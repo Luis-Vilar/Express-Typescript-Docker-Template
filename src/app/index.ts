1e744455e96eb2cc76982c8b622b263da064e513
Luis Vilar <luis_velazquez@etudante.sesisenai.org.br>
2024-09-15 14:23:03 -0300

    refactor server app to add dotenv suport

--- src/app/index.ts
+++ src/app/index.ts
@@ -1,20 +1,22 @@
-import express from 'express';
-import routes from '../routes';
-import { PORT } from '../utils/constants';
-
- class Server {
-  private app: express.Application;
+import express, { Application } from "express";
+import routes from "../routes";
+import { PORT } from "../utils/constants";
+import { config } from "dotenv";
+class Server {
+  private app: Application;
 
   constructor() {
+    config();
     this.app = express();
     this.app.use(express.json());
     this.app.use(routes);
   }
 
-  start(port: number, ) {
-    this.app.listen(port, ()=>console.log(`Servers is running on port ${port}`));
+  start(port: number) {
+    this.app.listen(port, () =>
+      console.log(`Servers is running on port ${port}`)
+    );
   }
 }
 
 export default new Server().start(PORT);
-
