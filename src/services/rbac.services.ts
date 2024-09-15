cc892dd38116dce2ac936704e02a7aaee11740ea
Luis Vilar <luis_velazquez@etudante.sesisenai.org.br>
2024-09-15 14:23:23 -0300

    Refactor rbac.services.ts to add async/await and improve RBAC middleware

--- src/services/rbac.services.ts
+++ src/services/rbac.services.ts
@@ -6,8 +6,8 @@ export async function getRoleId(role: $Enums.Role): Promise<number> {
   return (await prisma.roles.findFirst({ where: { role } })).roleId;
 }
 
-export function getFullRoleDetails(roleId: number) {
-  return prisma.roles.findFirst({
+export async function getFullRoleDetails(roleId: number) {
+  const roles = await prisma.roles.findFirst({
     where: {
       roleId,
     },
@@ -15,9 +15,39 @@ export function getFullRoleDetails(roleId: number) {
       permissions: true,
     },
   });
+  return roles;
 }
 
-export const rbacMiddleware = async (req: any, res: any, next: NextFunction) => {
-  console.log("RBAC Middleware");
-  next()
-}
+export const rbacMiddleware = (RBAC_REQUIRED: $Enums.Permision[]) => {
+  return async (req: any, res: any, next: NextFunction) => {
+    const userId = req.user.userId;
+
+    const user = await prisma.user.findFirst({
+      where: {
+        userId,
+      },
+      include: {
+        role: {
+          include: {
+            permissions: true,
+          },
+        },
+      },
+    });
+    const userPermissions = user.role.permissions.map(
+      (permissions) => permissions.permision
+    );
+
+    if (
+      !RBAC_REQUIRED.every((permission) => userPermissions.includes(permission))
+    ) {
+      return res.status(403).json({
+        message: "You do not have permission to access this resource",
+        required: RBAC_REQUIRED,
+        you_have: userPermissions,
+      });
+    }
+
+    next();
+  };
+};
