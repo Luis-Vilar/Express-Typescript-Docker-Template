a20e110c02c7afbe3bc977afb25e40053c2f8f75
Luis Vilar <luis_velazquez@etudante.sesisenai.org.br>
2024-09-15 14:24:47 -0300

    Refactor user routes to separate read operations

--- src/routes/v1/users.routes.ts
+++ src/routes/v1/users.routes.ts
@@ -2,12 +2,12 @@ import { Router } from "express";
 import { getAllUsers, createNewUser } from "../../controllers/user.controllers";
 
 const userPublicRouter = Router();
-const userPrivateRouter = Router();
+const userReadRoutes = Router();
 
 //PRIVATE ROUTES
-userPrivateRouter.get("/users", getAllUsers);
+userReadRoutes.get("/users", getAllUsers);
 
 //PUBLICS ROUTES
 userPublicRouter.post("/users", createNewUser);
 
-export { userPublicRouter, userPrivateRouter };
+export { userPublicRouter, userReadRoutes  };
