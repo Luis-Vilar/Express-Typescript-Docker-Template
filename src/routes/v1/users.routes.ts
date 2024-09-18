b3f881f7853494a0a0c6f0b520f4e90176dfa7fc
Luis Vilar <luis_velazquez@etudante.sesisenai.org.br>
2024-09-18 20:59:56 -0300

    Refactor users.routes.ts to include validation middleware for creating new users

--- src/routes/v1/users.routes.ts
+++ src/routes/v1/users.routes.ts
@@ -1,5 +1,7 @@
 import { Router } from "express";
 import { getAllUsers, createNewUser } from "../../controllers/user.controllers";
+import { UserCreateDTO } from "../../dtos/user.dto";
+import { validationMiddleware } from "../../services/classValidator.service";
 
 const userPublicRouter = Router();
 const userReadRoutes = Router();
@@ -8,6 +10,6 @@ const userReadRoutes = Router();
 userReadRoutes.get("/users", getAllUsers);
 
 //PUBLICS ROUTES
-userPublicRouter.post("/users", createNewUser);
+userPublicRouter.post("/users", validationMiddleware(UserCreateDTO) ,createNewUser);
 
 export { userPublicRouter, userReadRoutes  };
