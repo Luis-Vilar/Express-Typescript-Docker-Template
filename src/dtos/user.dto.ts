557c0e978819d149ff4e71b8f08accde5a3a56cf
Luis Vilar <luis_velazquez@etudante.sesisenai.org.br>
2024-09-18 20:59:41 -0300

    Refactor UserCreateDTO to include validation decorators

--- src/dtos/user.dto.ts
+++ src/dtos/user.dto.ts
@@ -1,4 +1,11 @@
 import { User } from "@prisma/client";
+import {
+  IsEmail,
+  IsNotEmpty,
+  IsString,
+  MinLength,
+  IsUUID,
+} from "class-validator";
 
 export class UserDTO implements User {
   userId: string;
@@ -8,7 +15,25 @@ export class UserDTO implements User {
   roleId: number;
 }
 
-export type UserCreateDTO = Omit<UserDTO, "userId">;
+export class UserCreateDTO implements Omit<UserDTO, "userId"> {
+  @IsString()
+  @IsNotEmpty()
+  name: string;
+
+  @IsEmail()
+  @IsNotEmpty()
+  email: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(6)
+  password: string;
+
+  roleId: number;
+};
+
+
+
 export type UserUpdateDTO = Partial<UserCreateDTO>;
 export type UserLoginDTO = Pick<UserDTO, "email" | "password">;
 export type UserResponseDTO = Omit<UserDTO, "password">;
