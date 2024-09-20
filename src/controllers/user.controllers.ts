006e269ee9b369ede79ab8c462191a6ff4fc71f2
Luis Vilar <luis_velazquez@etudante.sesisenai.org.br>
2024-09-20 15:58:22 -0300

    Refactor: Update user.controllers.ts

--- src/controllers/user.controllers.ts
+++ src/controllers/user.controllers.ts
@@ -1,18 +1,20 @@
-import { Request, Response } from "express";
-import {
-  getUserByEmail,
-  getUsers,
-  createUser,
-} from "../services/user.services";
-import { getRoleId } from "../services/rbac.services";
-
-import { hashSecret, compareSecret } from "../utils/bcrypt";
-import { generateToken } from "../services/jwt.services";
 import { $Enums } from "@prisma/client";
+import { Request, Response } from "express";
+import { UserCreateDTO, UserUpdateDTO } from "../dtos/user.dto";
 import { errorHandler } from "../services/error.services";
-import { UserCreateDTO } from "../dtos/user.dto";
+import { generateToken } from "../services/jwt.services";
+import { getRoleId } from "../services/rbac.services";
+import {
+  createUser,
+  getUserByEmail,
+  getUserById,
+  getUsers,
+  updateUser,
+} from "../services/user.services";
+import { RequestWithUser } from "../types";
+import { compareSecret, hashSecret } from "../utils/bcrypt";
 
-export const getAllUsers = async (req: any, res: Response) => {
+export const getAllUsers = async (req: Request, res: Response) => {
   const users = await getUsers();
   users?.length > 0
     ? res.status(200).json(users)
@@ -36,7 +38,7 @@ export const loginUser = async (req: Request, res: Response) => {
     res.status(200).json({
       message: "Login successful",
       token,
-      status: 200,
+      statusCode: 200,
     });
   } else {
     res.status(404).json({ message: "Invalid email or password" });
@@ -54,8 +56,33 @@ export const createNewUser = async (req: Request, res: Response) => {
       name,
       roleId,
     });
-    delete user.password
+    delete user.password;
     res.status(201).json({ message: "User created successfully", user });
+  } catch (error) {
+    errorHandler(error, res);
+  }
+};
+
+export const updateOneUser = async (req: RequestWithUser, res: Response) => {
+  try {
+    const { userId } = req.params;
+    const loggedUser = req.user;
+    if (loggedUser.sub !== userId) {
+      return res
+        .status(403)
+        .json({ message: "You are not authorized to update this user" });
+    }
+    const user = await getUserById(userId);
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+    const userUpdateDto: UserUpdateDTO = req.body;
+    const updatedUser = await updateUser(userId, userUpdateDto);
+    delete updatedUser.password;
+    res.status(200).json({
+      message: `User whit id ${userId} has updated`,
+      changes: userUpdateDto,
+    });
   } catch (error) {
     errorHandler(error, res);
   }
